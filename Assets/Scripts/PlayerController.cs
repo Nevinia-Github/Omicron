8f7951f8c9e1d8eb9896a4bcec20d27ef9f191f1
LavaPower <lavapower84@gmail.com>
2019-09-30 15:23:02 +0200

    Beginning of the game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float lookSpeed = 3f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        Vector3 _xMov = transform.right * Input.GetAxisRaw("Horizontal");
+        Vector3 _zMov = transform.forward * Input.GetAxisRaw("Vertical");
+        Vector3 _yMov = transform.up * Input.GetAxisRaw("Jump");
+        Vector3 _velocity = (_xMov + _zMov + _yMov).normalized * speed;
+
+        motor.SetVelocity(_velocity);
+
+        Vector3 _rotation = new Vector3(0, Input.GetAxisRaw("Mouse X"), 0) * lookSpeed;
+
+        motor.SetRotation(_rotation);
+
+        Vector3 _cameraRotation = new Vector3(Input.GetAxisRaw("Mouse Y"), 0, 0) * lookSpeed;
+
+        motor.SetCameraRotation(_cameraRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private Vector3 cameraRotation;
+
+    private Rigidbody rb;
+
+    public void SetVelocity(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void SetRotation(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void SetCameraRotation(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void Rotate()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        print(cam.transform.eulerAngles[0]);
+        if (cameraRotation[0] < 0)
+        {
+            if (cam.transform.eulerAngles[0] <= 50f || cam.transform.eulerAngles[0] >= 100f)
+                cam.transform.Rotate(-cameraRotation);
+        }
+        else if (cameraRotation[0] > 0)
+        {
+            if (cam.transform.eulerAngles[0] <= 200f || cam.transform.eulerAngles[0] >= 330f)
+                cam.transform.Rotate(-cameraRotation);
+        }
+    }
+}
